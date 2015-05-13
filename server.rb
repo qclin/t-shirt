5077c3fa31da40e47b6a2ec7510b8944b737c37f
Qiao Lin <qiaoclin@gmail.com>
2015-05-13 12:44:02 -0400

    server get/post routes

--- server.rb
+++ server.rb
@@ -9,16 +9,40 @@ end
 
 #Display all tshirts on main Page
 get "/tshirts" do
-	tshirts = Tshirts.all
+	tshirts = Tshirt.all
 	erb :index, locals: {tshirts: tshirts}			##ADD HERE
-	end
+end
 
 #Display individual page
 get "/tshirts/:id" do
-	erb :show_shirt
+	one_shirt = Tshirt.find(params[:id])
+	erb :show_shirt, locals: {tshirt: one_shirt}
 end
 
-#
+#show new sales form 
+get "/tshirts/:id/newSale"
+	one_shirt = Tshirt.find(params[:id])
+	erb :new_sale, locals: {tshirt: one_shirt}
+end 
+
+#posting to sales table 
+post "/tshirts/:id/newSale"
+	email = params[:email]
+	tshirt_id = params[:id]
+	quantity = params[:quantity].to_i
+	# now update tshirt quantity 
+	this_shirt = Tshirt.find(tshirt_id)
+	new_quantity = this_shirt.quantity_available - quantity
+	
+	if new_quantity =< 0 
+		response = "Sorry we don't have that many t-shirt for you bugger off"
+	else 
+		Sale.create({user_email: email, tshirt_id: tshirt_id, quantity_purchased: quantity})
+		this_shirt.update({quantity_available: new_quantity})
+		redirect('/tshirts/confirmation')
+	end 
+end
+
 
 
 
