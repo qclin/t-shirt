38e59f63a7892e15fe8f64d13ebaf67a239bbcb0
Steve McIntosh <scm.phub@gmail.com>
2015-05-13 14:22:13 -0400

    get initial page up; console.rb, .gitignore

---
+++ console.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require './lib/connection'
+require './models/sale'
+require './models/tshirt'
+
+#me = Citizen.find_by(name: "Crawford")
+#me.state
+
+#nj = State.find(1)
+#nj.citizens
+#nj.citizens.create(name: "Edison")
+
+
+binding.pry
